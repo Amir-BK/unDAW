a036d784adb0a0561cc6aae8c37a4ef0e7833666
Amir BK <Amir.BK@gmail.com>
2024-04-06 07:18:55 +0300

    Cleanup broken assets that slow down project load

---
+++ Source/BKEditorUtilities/Public/SessionMapper/SessionMapperEditorWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "SessionMapperEditorWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BK_EDITORUTILITIES_API USessionMapperEditorWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
