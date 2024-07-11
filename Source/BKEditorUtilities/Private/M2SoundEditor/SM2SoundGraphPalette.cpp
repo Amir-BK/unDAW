e0c996fa229f03c69f00d874017b8d0f2d557efe
Amir-BK <Amir.BK@gmail.com>
2024-07-11 15:37:31 +0300

    Some boilerplates and cleanup

---
+++ Source/BKEditorUtilities/Private/M2SoundEditor/SM2SoundGraphPalette.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "M2SoundEditor/SM2SoundGraphPalette.h"
+
+SM2SoundGraphPalette::SM2SoundGraphPalette()
+{
+}
+
+SM2SoundGraphPalette::~SM2SoundGraphPalette()
+{
+}
