1b7d143061586828ac682b0a5da39bbcc4dc07f8
Amir-BK <Amir.BK@gmail.com>
2025-05-19 17:46:39 +0300

    Update unDAW_ConfigurableMixer.h
    remove version checking from configurable mixer class cause it crashes in 5_6, epic really don't like version checking

--- Source/unDAWMetaSounds/Public/Nodes/unDAW_ConfigurableMixer.h
+++ Source/unDAWMetaSounds/Public/Nodes/unDAW_ConfigurableMixer.h
@@ -14,10 +14,7 @@
 #include "unDAW_ConfigurableMixer.generated.h"
 
 USTRUCT()
-struct FConfigurableMixerConfiguration 
-#if (ENGINE_MAJOR_VERSION >= 5) && (ENGINE_MINOR_VERSION >= 6)	//as the metasound configuration struct is only available in 5.6 and later we do some weird version checking here
-	:public FMetaSoundFrontendNodeConfiguration
-#endif
+struct FConfigurableMixerConfiguration  :public FMetaSoundFrontendNodeConfiguration
 {
 	GENERATED_BODY()
 
@@ -29,7 +26,6 @@ struct FConfigurableMixerConfiguration
 
 	UPROPERTY(EditAnywhere, Category = General, meta = (DisplayName = "Equal Power Panning Law"))
 	bool bEqualPower;
-#if (ENGINE_MAJOR_VERSION >= 5) && (ENGINE_MINOR_VERSION >= 6)	
 
 	FConfigurableMixerConfiguration();
 
@@ -38,7 +34,7 @@ struct FConfigurableMixerConfiguration
 
 	///* Pass data down to the operator. */
 	virtual TSharedPtr<const Metasound::IOperatorData> GetOperatorData() const override;
-#endif
+
 };
 
 
